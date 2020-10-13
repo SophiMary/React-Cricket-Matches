bb2a16aee782aad39aa67ee13acea9938e4236d1
SophiMary <sofy.mary@outlook.com>
2020-10-13 09:44:12 +0530

    Updates

---
+++ precache-manifest.6bc60005e43b604d5edfb9ab895955ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab890db7c608a784989b611f84bc3a6b",
+    "url": "/React-Cricket-Matches/index.html"
+  },
+  {
+    "revision": "4c9ca1fd36bc932d362b",
+    "url": "/React-Cricket-Matches/static/css/main.182cf78b.chunk.css"
+  },
+  {
+    "revision": "fc0a20f64ce23e1da936",
+    "url": "/React-Cricket-Matches/static/js/2.f13124ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Cricket-Matches/static/js/2.f13124ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9ca1fd36bc932d362b",
+    "url": "/React-Cricket-Matches/static/js/main.d968f39a.chunk.js"
+  },
+  {
+    "revision": "80f7a50bf0ede3e6ac49",
+    "url": "/React-Cricket-Matches/static/js/runtime-main.94d9893e.js"
+  },
+  {
+    "revision": "98c6db574b1685a03cafa01af3f220b4",
+    "url": "/React-Cricket-Matches/static/media/vs.98c6db57.jpg"
+  }
+]);
